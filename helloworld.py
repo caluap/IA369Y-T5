5100bd3f424a71d946e541b032cf525c38e6ffef
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2017-10-24 14:55:05 -0200

    exemplo 1: postar uma mensagem numa conta (tirado do tut. da digital ocean)

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+import tweepy
+import keys
+
+# Create variables for each key, secret, token
+auth = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
+auth.set_access_token(keys.access_token, keys.access_token_secret)
+api = tweepy.API(auth)
+
+# Set up OAuth and integrate with API
+auth = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
+auth.set_access_token(keys.access_token, keys.access_token_secret)
+api = tweepy.API(auth)
+
+# Write a tweet to push to our Twitter account
+tweet = 'apparently, status can\'t be duplicates'
+api.update_status(status=tweet)
