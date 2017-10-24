f89390f29ae835eb3dabb4fbfadc42f058cec7b6
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2017-10-24 15:09:12 -0200

    busca por uma query

--- helloworld.py
+++ helloworld.py
@@ -12,5 +12,10 @@ auth.set_access_token(keys.access_token, keys.access_token_secret)
 api = tweepy.API(auth)
 
 # Write a tweet to push to our Twitter account
-tweet = 'apparently, status can\'t be duplicates'
-api.update_status(status=tweet)
+# tweet = 'apparently, status can\'t be duplicates'
+# api.update_status(status=tweet)
+
+results = api.search(q="\"me sentindo\"", lang="pt")
+
+for result in results:
+    print(result)
